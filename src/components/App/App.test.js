16f1c10aa75d46159eda870c0ca5f54e19be12a6
Ryan Watkins <rwatkins@esri.com>
2021-06-22 21:19:20 -0700

    simple test suite

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import Header from '../Header';
+import UserList from '../UserList';
+
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+describe('render main components', () => {
+  it('renders the App component', () => {
+    shallow(<App />);
+  });
+
+  it('renders the Header component within the App component', () => {
+    const container = shallow(<App />);
+
+    expect(container.containsMatchingElement(<Header />)).toEqual(true);
+  });
+
+  it('renders the UserList component within the App component', () => {
+    const container = shallow(<App />);
+
+    expect(container.containsMatchingElement(<UserList />)).toEqual(true);
+  });
+});
+
+describe('snapshots', () => {
+  it('App snapshot', () => {
+    const snap = shallow(<App />);
+    expect(toJson(snap)).toMatchSnapshot();
+  });
 });
